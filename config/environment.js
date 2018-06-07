01fabf4498bec968fe9c0f8a51ea0d476581c019
jakevo <javoucsd@gmail.com>
2018-06-06 22:30:22 -0700

    deploy to gh

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   let ENV = {
-    modulePrefix: 'https://github.com/jakevo/TechTrisTeam',
+    modulePrefix: 'tech-tris',
     environment,
     rootURL: '/',
     locationType: 'auto',
@@ -44,8 +44,6 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.locationType = 'hash';
-    ENV.rootURL = '/tech-tris/';
     // here you can enable a production-specific feature
   }
 
